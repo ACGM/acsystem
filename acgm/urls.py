183bc72b3ff1268795a4e78b4478f1b39661f6fb
Rafael Mercedes <rafaelmersant@gmail.com>
2014-12-12 20:50:08 -0400

    Inicio de modulo Fondos de Cajas (Desembolsos)

--- acgm/urls.py
+++ acgm/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from fondoscajas.views import DesembolsoView
+
+
 admin.site.site_header = 'COOPERATIVA'
 
 urlpatterns = patterns('',
 
     url(r'^$', 'acgm.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^desembolso/$', DesembolsoView.as_view(), name='Desembolso'),
+
 )
