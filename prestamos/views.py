1bec7402459963a65bfe4cb5a92d3fc80253067f
Rafael Mercedes <rafaelmersant@gmail.com>
2014-12-15 18:52:28 -0400

    Diseño Nota de Debito terminado, Maestra de Prestamos 40%

--- prestamos/views.py
+++ prestamos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+#Vista para Notas de Debito
+class NotaDeDebitoView(TemplateView):
+
+	template_name = 'notasdebito.html'
+
+
+#Vista para Maestra de Prestamos
+class MaestraPrestamosView(TemplateView):
+
+	template_name = 'maestraprestamos.html'
