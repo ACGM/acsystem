d061506d6b034ca6c69f78ef0a75ef0b4de3461e
Rafael Mercedes <rafaelmersant@gmail.com>
2015-04-14 19:38:40 -0400

    Fue corregido el contextProcessor para aceptar en el lado admin sin tener localidad asignada en el usuario.

--- acgm/context_processors.py
+++ acgm/context_processors.py
@@ -8,32 +8,36 @@ from administracion.models import UserExtra, Perfil, Opcion
 # amarrados a un perfil de usuario.
 def menu(request):
 	
-	loc = request.session.get('localidad')
-	usuario = UserExtra.objects.get(usuario__username=request.user.username)
-	perfilusr = Perfil.objects.get(perfilCod=usuario.perfil.perfilCod)
+	try:
+		loc = request.session.get('localidad')
+		usuario = UserExtra.objects.get(usuario__username=request.user.username)
+		perfilusr = Perfil.objects.get(perfilCod=usuario.perfil.perfilCod)
 
-	opciones = Opcion.objects.filter(perfil=perfilusr)
+		opciones = Opcion.objects.filter(perfil=perfilusr)
 
-	return {
-		'localidad': loc,
-		opciones[0].descripcion : opciones[0].valor,
-		opciones[1].descripcion : opciones[1].valor,
-		opciones[2].descripcion : opciones[2].valor,
-		opciones[3].descripcion : opciones[3].valor,
-		opciones[4].descripcion : opciones[4].valor,
-		opciones[5].descripcion : opciones[5].valor,
-		opciones[6].descripcion : opciones[6].valor,
-		opciones[7].descripcion : opciones[7].valor,
-		opciones[8].descripcion : opciones[8].valor,
-		opciones[9].descripcion : opciones[9].valor,
-		opciones[10].descripcion : opciones[10].valor,
-		opciones[11].descripcion : opciones[11].valor,
-		opciones[12].descripcion : opciones[12].valor,
-		opciones[13].descripcion : opciones[13].valor,
-		opciones[14].descripcion : opciones[14].valor,
-		opciones[15].descripcion : opciones[15].valor,
-	} 
-	
+		return {
+			'localidad': loc,
+			opciones[0].descripcion : opciones[0].valor,
+			opciones[1].descripcion : opciones[1].valor,
+			opciones[2].descripcion : opciones[2].valor,
+			opciones[3].descripcion : opciones[3].valor,
+			opciones[4].descripcion : opciones[4].valor,
+			opciones[5].descripcion : opciones[5].valor,
+			opciones[6].descripcion : opciones[6].valor,
+			opciones[7].descripcion : opciones[7].valor,
+			opciones[8].descripcion : opciones[8].valor,
+			opciones[9].descripcion : opciones[9].valor,
+			opciones[10].descripcion : opciones[10].valor,
+			opciones[11].descripcion : opciones[11].valor,
+			opciones[12].descripcion : opciones[12].valor,
+			opciones[13].descripcion : opciones[13].valor,
+			opciones[14].descripcion : opciones[14].valor,
+			opciones[15].descripcion : opciones[15].valor,
+		} 
+	except Exception as e:
+		if request.get_full_path()[:7] == '/admin/':
+			return {'localidad': 'None'}
+		pass
 
 	# if request.user != 'AnonymousUser':
 
