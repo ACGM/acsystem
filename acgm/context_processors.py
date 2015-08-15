d97186ca70611bcbb576f0d60640703973aaec19
Rafael Mercedes <rafaelmersant@gmail.com>
2015-08-15 10:41:42 -0400

    Las opciones de menu son llenadas de forma dinamica en un objecto de tipo diccionario en el context_processor.

--- acgm/context_processors.py
+++ acgm/context_processors.py
@@ -15,25 +15,13 @@ def menu(request):
 
 		opciones = Opcion.objects.filter(perfil=perfilusr)
 
-		return {
-			'localidad': loc,
-			# opciones[0].descripcion : opciones[0].valor,
-			# opciones[1].descripcion : opciones[1].valor,
-			# opciones[2].descripcion : opciones[2].valor,
-			# opciones[3].descripcion : opciones[3].valor,
-			# opciones[4].descripcion : opciones[4].valor,
-			# opciones[5].descripcion : opciones[5].valor,
-			# opciones[6].descripcion : opciones[6].valor,
-			# opciones[7].descripcion : opciones[7].valor,
-			# opciones[8].descripcion : opciones[8].valor,
-			# opciones[9].descripcion : opciones[9].valor,
-			# opciones[10].descripcion : opciones[10].valor,
-			# opciones[11].descripcion : opciones[11].valor,
-			# opciones[12].descripcion : opciones[12].valor,
-			# opciones[13].descripcion : opciones[13].valor,
-			# opciones[14].descripcion : opciones[14].valor,
-			# opciones[15].descripcion : opciones[15].valor,
-		} 
+		data = {'localidad': loc}
+
+		for op in opciones:
+			data[op.descripcion] = op.valor
+
+		return data
+		
 	except Exception as e:
 		if request.get_full_path()[:7] == '/admin/':
 			return {'localidad': 'None'}
