cb8acdab2faa125e168884a1f92d61174fd39586
Yeison Segura <yeisp1011@gmail.com>
2015-11-06 15:54:20 -0400

    Modificado los registros de CXP para ordenes y para supercoop

--- cxp/admin.py
+++ cxp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from cxp.models import OrdenCompra, CxpSuperCoop
+from cxp.models import OrdenCompra, CxpSuperCoop, OrdenGeneral, OrdenDetalleFact, cxpSuperDetalle, cxpSuperGeneral
 
 @admin.register(OrdenCompra)
 class OrdenCompraAdmin(admin.ModelAdmin):
@@ -10,3 +10,20 @@ class OrdenCompraAdmin(admin.ModelAdmin):
 @admin.register(CxpSuperCoop)
 class OrdenSuperAdmin(admin.ModelAdmin):
     list_display = ['id', 'suplidor', 'factura', 'concepto', 'fecha', 'monto', 'descuento']
+
+
+@admin.register(OrdenDetalleFact)
+class OrdenDetalleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'idRegistro', 'factura', 'fecha', 'monto']
+
+@admin.register(OrdenGeneral)
+class OrdenGeneralAdmin(admin.ModelAdmin):
+    list_display = ['id', 'suplidor', 'concepto', 'fecha', 'monto', 'descuento']
+
+@admin.register(cxpSuperGeneral)
+class cxpSuperGeneralAdmin(admin.ModelAdmin):
+	list_display = ['id', 'suplidor', 'concepto', 'fecha', 'monto', 'descuento']
+
+@admin.register(cxpSuperDetalle)
+class cxpSuperDetAdmin(admin.ModelAdmin):
+	list_display = ['id','idRegistro', 'fecha', 'monto']
